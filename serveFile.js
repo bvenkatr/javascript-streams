47b361aa0049a5431809e96da8b79f5d7e16632b
Venkat <venkat.jaa@gmail.com>
2015-12-09 18:34:56 +0530

    Read file and send to client

--- serveFile.js
+++ serveFile.js
@@ -1,10 +1,14 @@
 var http = require('http'),
 		fs = require('fs');
 
-var handler = function(req, res){
-	res.write("This is response from my server application");
-	res.end();
+var handler = function(request, response){
+	fs.readFile(__dirname + '/node_modules/express/lib/response.js', function(err, data){
+		response.end(data);
+	});
+
 };
+//
+// The app.listen function takes an optional callback, which is called once the app is ready to start taking requests
 
 http.createServer(handler).listen(7777, function(){
 	console.log("Server is listening at port 7777");

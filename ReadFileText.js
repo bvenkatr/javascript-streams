0b11bdca340241516c99a0a553b4fe3c8b2fc367
Venkat <venkat.jaa@gmail.com>
2016-03-24 16:16:01 +0530

    Write data to another file

--- ReadFileText.js
+++ ReadFileText.js
@@ -10,17 +10,26 @@ var data = '';
 
 //Create a readable stream from the given input
 var readerStream = fs.createReadStream('./Docs/streams.md');
+var writableStream = fs.createWriteStream('output.txt');
 
 // Handle stream events --> data, end, and error
 readerStream.on('data',  function(result){
 	data += result;
 });
-
 readerStream.on('end', function(){
-	console.log("End => Reading file has Done");
-	console.log(data);
-});
 
+	// Now pass data to writable stream
+	writableStream.write(data);
+
+});
 readerStream.on('error', function(error){
 	console.log("error", error);
+});
+
+writableStream.on('finish', function(){
+	console.log('Write Completed');
+});
+
+writableStream.on('error', function(error){
+	console.log(error.stack);
 });
